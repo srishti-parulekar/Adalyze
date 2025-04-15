c6543c0c95648d3a97172a77cb9b731e6ecbcf7b
Srishti Parulekar <srishtiparulekar430@gmail.com>
2025-04-15 14:26:15 +0530

    set up power bi client

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-
+import React from "react";
+import { PowerBIEmbed } from "powerbi-client-react";
+import { models } from "powerbi-client";
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <>
+      <div>Dashboard</div>
+      <PowerBIEmbed
+        embedConfig={{
+          type: "report", // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+          id: "<Report Id>",
+          embedUrl: "<Embed Url>",
+          accessToken: "<Access Token>",
+          tokenType: models.TokenType.Embed, // Use models.TokenType.Aad for SaaS embed
+          settings: {
+            panes: {
+              filters: {
+                expanded: false,
+                visible: false,
+              },
+            },
+            background: models.BackgroundType.Transparent,
+          },
+        }}
+        eventHandlers={
+          new Map([
+            [
+              "loaded",
+              function () {
+                console.log("Report loaded");
+              },
+            ],
+            [
+              "rendered",
+              function () {
+                console.log("Report rendered");
+              },
+            ],
+            [
+              "error",
+              function (event) {
+                console.log(event.detail);
+              },
+            ],
+            ["visualClicked", () => console.log("visual clicked")],
+            ["pageChanged", (event) => console.log(event)],
+          ])
+        }
+        cssClassName={"reportClass"}
+        getEmbeddedComponent={(embeddedReport) => {
+          window.report = embeddedReport;
+        }}
+      />
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
