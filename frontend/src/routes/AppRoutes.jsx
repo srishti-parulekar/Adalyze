91e529168cec93073f27460944a0300417284772
Srishti Parulekar <srishtiparulekar430@gmail.com>
2025-04-17 20:14:46 +0530

    set up prediction page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes} from "react-router-dom";
 import LandingPage from '../pages/LandingPage';
 import Dashboard from '../pages/Dashboard';
+import PredictionPage from '../pages/PredictionPage';
 
 const AppRoutes = () => {
   return (
     <Routes>
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/predict' element={<PredictionPage />} />
         <Route path='/' element={<LandingPage/>} />
     </Routes>
   )
